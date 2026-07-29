8f387b81b14581165afef21773724eec81c5e455
moshesoffer <moshesoffer@gmail.com>
2026-07-29 12:23:05 +0300

    add push/pull bug-tracker bat files

---
+++ bug-tracker/pull.bat
@@ -0,0 +1,2 @@
+git fetch origin
+git restore --source origin/main -- UBA-bugs-tracker.xlsx
---
+++ bug-tracker/push.bat
@@ -0,0 +1,3 @@
+git add -f UBA-bugs-tracker.xlsx
+git commit -m"bug tracker update"
+git push origin main
